142efc6a630ec267d524623c85fd88c907112726
RAHIOUI Zakaria <S610907@siege.axa-fr.intraxa>
2016-12-08 17:44:10 +0100

    First commit

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringSumKata
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+using StringSumKata;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {            
+            var args = new string[] {};
+            Program.Main(args);
+
+            Assert.True(true);
+        }
+    }
+}
