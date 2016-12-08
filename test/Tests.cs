cd0b7269c02b033f717ac52afd8fa07ce5cda907
Zakaria <Zakaria.ra@gmail.com>
2016-12-09 00:50:22 +0100

    The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0).

--- test/Tests.cs
+++ test/Tests.cs
@@ -1,18 +1,70 @@
 ﻿using System;
 using Xunit;
 using StringSumKata;
+using System.IO;
 
 namespace Tests
 {
-    public class Tests
+    public class Tests : IDisposable
     {
-        [Fact]
-        public void Test1() 
-        {            
-            var args = new string[] {};
-            Program.Main(args);
 
-            Assert.True(true);
+        private StringWriter consoleOut;
+        private string[] arguments;
+
+        public Tests()
+        {
+            consoleOut = new StringWriter();
+            Console.SetOut(consoleOut);
+
+            
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        [Fact]    
+        public void Retuns_0_if_empty_arguments() 
+        {  
+            // Arrange 
+            arguments = new string[]{};
+            // Act 
+            Program.Main(arguments);
+            
+            // Assert 
+            Assert.Equal(0, int.Parse(consoleOut.ToString()));
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        [InlineData("33", 33)]
+        [InlineData("102", 102)]
+        public void Returns_int_value_of_string_when_one_argument(string x, int y)
+        {
+            //Given
+            arguments = new string[]{x};
+            
+            //When
+            Program.Main(arguments);   
+
+            //Then
+            Assert.Equal(y, int.Parse(consoleOut.ToString()));
+        }
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        public void Returns_sum_of_two_numbers_separated_by_comma(string x, int y)
+        {
+            //Given
+            arguments = new string[]{x};
+            
+            //When
+            Program.Main(arguments);   
+
+            //Then
+            Assert.Equal(y, int.Parse(consoleOut.ToString()));
         }
     }
 }
